5ece103c503fc53ff03e0932330c8846276c5bba
vegafalomirruben <vegafalomirruben@gmail.com>
2026-01-26 23:04:00 +0100

    desdecasa6

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,7 +5,7 @@
 
 //export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-/*MODIFICACION2
+
 import { createClient } from '@supabase/supabase-js'
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -20,24 +20,5 @@ if (!url || !key) {
     throw new Error(`Error de Configuración: URL=${url}, KEY=${key}`);
 }
 
-export const supabase = createClient(url, key)*/
-
-// src/lib/supabase.ts
-import { createClient } from '@supabase/supabase-js';
-
-export function getSupabaseClient() {
-    const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-    console.log("-----------------------------------------");
-    console.log("VALOR URL:", url ? "Detectada ✅" : "VACÍA ❌");
-    console.log("VALOR KEY:", key ? "Detectada ✅" : "VACÍA ❌");
-    console.log("-----------------------------------------");
-
-    if (!url || !key) {
-        throw new Error(`Error de Configuración: URL=${url}, KEY=${key}`);
-    }
-
-    return createClient(url, key);
-}
+export const supabase = createClient(url, key)
 
